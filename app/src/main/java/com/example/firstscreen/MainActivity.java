0ad46214d9bc21988d46fd01fdd7963244a06902
notebookghost <notebookghost@icloud.com>
2021-02-24 08:14:33 +0400

    otp model

---
+++ app/src/main/java/com/example/firstscreen/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.firstscreen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mLogOutBtn;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        mLogOutBtn = findViewById(R.id.log_out_btn);
+        mAuth = FirebaseAuth.getInstance();
+
+        mLogOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                startActivity(new Intent(MainActivity.this , SecondScreen.class));
+                finish();
+            }
+        });
+
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser == null){
+            startActivity(new Intent(MainActivity.this , SecondScreen.class));
+            finish();
+        }
+    }
+}
