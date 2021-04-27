b712c8ddc4bbc1d4b29ce900d83821ca1a7c7aa4
notebookghost <notebookghost@icloud.com>
2021-04-27 17:35:08 +0400

    yes

--- app/src/main/java/com/example/firstscreen/MainActivity.java
+++ app/src/main/java/com/example/firstscreen/MainActivity.java
@@ -2,11 +2,15 @@ package com.example.firstscreen;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -19,6 +23,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomeFragment()).commit();
+
 
 
         mLogOutBtn = findViewById(R.id.log_out_btn);
@@ -36,6 +47,39 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    private  BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+
+                        case R.id.nav_chat:
+                            selectedFragment = new ChatFragment();
+                            break;
+
+                        case R.id.nav_post:
+                            selectedFragment = new PostFragment();
+                            break;
+
+                        case R.id.nav_myads:
+                            selectedFragment = new MyadsFragment();
+                            break;
+
+                        case R.id.nav_profile:
+                            selectedFragment = new ProfileFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+
+                    return true;
+                }
+            };
 
     @Override
     protected void onStart() {
