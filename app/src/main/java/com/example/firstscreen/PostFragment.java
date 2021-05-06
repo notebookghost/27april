8fe15197bc1bf40931413169791619274f7bca81
notebookghost <notebookghost@icloud.com>
2021-05-06 20:20:32 +0400

    ok

--- app/src/main/java/com/example/firstscreen/PostFragment.java
+++ app/src/main/java/com/example/firstscreen/PostFragment.java
@@ -4,16 +4,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PostFragment extends Fragment {
+
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_post,container,false);
+
+
     }
 }
